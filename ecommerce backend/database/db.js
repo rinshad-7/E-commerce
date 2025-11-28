5a5bc845c2a0ca00899668a372659d6e293d9a93
rinshad <rinshadmelethalakkal123@gmail.com>
2025-11-28 19:12:50 +0530

    changed mongo string

--- ecommerce backend/database/db.js
+++ ecommerce backend/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
  export async function connectDb (){
     try{
-       await mongoose.connect("mongodb+srv://rinshadmelethalakkal123_db_user:6MY31ixFTpuI9qmg@cluster0.i9tpogi.mongodb.net/?appName=Cluster0/clotheCommerce")
+       await mongoose.connect("mongodb+srv://rinshaddb:1234@cluster0.i9tpogi.mongodb.net/?appName=Cluster0/clotheCommerce")
        console.log("database connection successfull");
        
     }catch(err){
@@ -11,5 +11,4 @@ import mongoose from "mongoose";
     }
 }
 
-connectDb()
 

4a71493afdd967f7e83c866eee94e7cb307638aa
rinshad <rinshadmelethalakkal123@gmail.com>
2025-11-28 12:00:36 +0530

    3000

--- react_project/ecommerce/src/api/api.jsx
+++ react_project/ecommerce/src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:300/", 
+  baseURL: "http://localhost:3000/", 
   withCredentials:true,
 });
 
